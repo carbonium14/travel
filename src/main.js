95153cc9a2e55d82b6a65e89962ce07509eac944
carbonium14 <1075413520@qq.com>
2023-01-01 13:43:31 +0800

    finish framework

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import FastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 Vue.config.productionTip = false
-
+FastClick.attach(document.body)
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    home
   </div>
 </template>
 
 <script>
 export default {
-  name: 'APP'
+  name: 'HOME'
 }
 </script>
 
