1b20bb6e0461c9016b7ba8e0fc3f556ed23f7caa
carbonium14 <1075413520@qq.com>
2023-01-03 11:51:33 +0800

    add recommend

---
+++ src/views/Home/components/Recommend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <img class="item-image" :src="item.imgUrl" alt="图片"/>
+            <div class="item-info">
+                <p class="item-title">{{ item.title }}</p>
+                <p class="item-desc">{{ item.desc }}</p>
+                <button class="item-button">查看详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: '',
+        title: '极地世界',
+        desc: '极地世界真好玩'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+.title {
+    margin-top: .2rem;
+    line-height: .8rem;
+    background-color: #eee;
+    text-indent: .2rem;
+}
+.item {
+    display: flex;
+    height: 1.9rem;
+    overflow: hidden;
+    .item-image {
+        width: 1.7rem;
+        height: 1.7rem;
+        padding: .1rem;
+    }
+    .item-info {
+        flex: 1;
+        padding: .1rem;
+        min-width: 0;
+        .item-title {
+            line-height: .54rem;
+            font-size: .32rem;
+            ellpsis();
+        }
+        .item-desc {
+            line-height: .4rem;
+            color: #ccc;
+            ellpsis();
+        }
+        .item-button {
+            line-height: .44rem;
+            margin-top: .16rem;
+            background-color: #ff9300;
+            padding: 0 .2rem;
+            border-radius: .06rem;
+            color: #fff;
+        }
+    }
+}
+</style>
---
+++ src/views/Home/components/Weekend.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-image-wrapper">
+                    <img class="item-image" :src="item.imgUrl" alt="图片"/>
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{ item.title }}</p>
+                    <p class="item-desc">{{ item.desc }}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: '',
+        title: '极地世界',
+        desc: '极地世界真好玩'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+.title {
+    margin-top: .2rem;
+    line-height: .8rem;
+    background-color: #eee;
+    text-indent: .2rem;
+}
+.item-image-wrapper {
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 33.9%;
+    .item-image {
+        width: 100%;
+    }
+}
+.item-info {
+    padding: .1rem;
+    .item-title {
+        line-height: .54rem;
+        font-size: .32rem;
+        ellpsis();
+    }
+    .item-desc {
+        line-height: .4rem;
+        color: #ccc;
+        ellpsis();
+    }
+}
+</style>
