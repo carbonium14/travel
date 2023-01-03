1e7129485759e9857603cb6b68705dd912628ca7
carbonium14 <1075413520@qq.com>
2023-01-03 14:27:45 +0800

    ajax city

--- src/views/City/City.vue
+++ src/views/City/City.vue
@@ -2,8 +2,8 @@
   <div>
     <CityHeader></CityHeader>
     <CitySearch></CitySearch>
-    <CityList></CityList>
-    <CityAlphabet></CityAlphabet>
+    <CityList :list="cities" :hot="hotCities"></CityList>
+    <CityAlphabet :list="cities"></CityAlphabet>
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
 import CityAlphabet from './components/Alphabet.vue'
+// import axios from 'axios'
 export default {
   name: 'CITY',
   components: {
@@ -19,6 +20,68 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {
+        A: [{
+          id: 56,
+          spell: 'aba',
+          name: '阿坝'
+        }],
+        B: [{
+          id: 1,
+          spell: 'beijing',
+          name: '北京'
+        }],
+        C: [{
+          id: 2,
+          spell: 'chongqing',
+          name: '重庆'
+        }]
+      },
+      hotCities: [{
+        id: 1,
+        spell: 'beijing',
+        name: '北京'
+      }, {
+        id: 3,
+        spell: 'shanghai',
+        name: '上海'
+      }, {
+        id: 47,
+        spell: 'xian',
+        name: '西安'
+      }, {
+        id: 239,
+        spell: 'sanya',
+        name: '三亚'
+      }, {
+        id: 188,
+        spell: 'lijiang',
+        name: '丽江'
+      }, {
+        id: 125,
+        spell: 'guilin',
+        name: '桂林'
+      }]
+    }
+  },
+  methods: {
+    // getCityInfo () {
+    //   axios.get('/static/mock/city.json').then(this.handlerGetCityInfoSucc)
+    // },
+    // handlerGetCityInfoSucc (res) {
+    //   res = res.data
+    //   if (res.ret && res.data) {
+    //     const data = res.data
+    //     this.cities = data.cities
+    //     this.hotCities = data.hotCities
+    //   }
+    // }
+  },
+  mounted () {
+    // this.getCityInfo()
   }
 }
 </script>
--- src/views/City/components/Alphabet.vue
+++ src/views/City/components/Alphabet.vue
@@ -1,15 +1,17 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) of list" :key="key">{{ key }}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    list: {
+      type: Object
+    }
+  }
 }
 </script>
 
