3b89dc90cd37cbced4abc0e4e01d57fbf5dc7e4b
carbonium14 <1075413520@qq.com>
2023-01-04 13:44:37 +0800

    cli-upgrade

--- src/views/detail/components/Banner.vue
+++ src/views/detail/components/Banner.vue
@@ -71,7 +71,7 @@ export default {
         bottom: 0;
         line-height: .6rem;
         color: #fff;
-        background-image: linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8));
+        background-image: linear-gradient(to top, rgba(0,0,0,0), rgba(0,0,0,0.8));
         .banner-title {
             font-size: .32rem;
             padding: 0 .2rem;
